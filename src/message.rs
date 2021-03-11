0e6f5fdaae69aea2cb3b4d3c0b289ade67fa4081
Anton Gushcha <ncrashed@protonmail.com>
2021-03-12 01:29:45 +0300

    Define fields of messages

--- src/message.rs
+++ src/message.rs
@@ -1,4 +1,5 @@
 use std::net;
+use fixed::{types::extra::U7, FixedU64};
 
 /// Currencies that protocol aware of, there can be currencies that will never be implemented but
 /// index in protocol is known.
@@ -61,6 +62,32 @@ impl Currency {
     }
 }
 
+#[derive(Clone, Copy, PartialEq, Eq, Debug)]
+pub enum Fiat {
+    Usd,
+    Eur,
+    Rub,
+}
+
+impl Fiat {
+    pub fn to_index(&self) -> u32 {
+        match self {
+            Fiat::Usd => 0,
+            Fiat::Eur => 1,
+            Fiat::Rub => 2,
+        }
+    }
+
+    pub fn from_index(i: u32) -> Option<Self> {
+        match i {
+            0 => Some(Fiat::Usd),
+            1 => Some(Fiat::Eur),
+            2 => Some(Fiat::Rub),
+            _ => None,
+        }
+    }
+}
+
 #[derive(Clone, PartialEq, Eq, Debug)]
 pub enum Address {
     AddressIpv4(net::SocketAddrV4),
@@ -86,42 +113,127 @@ pub enum Message {
     Rates(Vec<RateResp>),
 }
 
+impl Message {
+    pub fn id(&self) -> u32 {
+        match self {
+            Message::Version(_) => 0,
+            Message::VersionAck => 1,
+            Message::GetFilters(_) => 2,
+            Message::Filters(_) => 3,
+            Message::Filter(_) => 4,
+            Message::GetPeers => 5,
+            Message::Peers(_) => 6,
+            Message::GetFee(_) => 7,
+            Message::Fee(_) => 8,
+            Message::PeerIntroduce(_) => 9,
+            Message::Reject(_) => 10,
+            Message::Ping(_) => 11,
+            Message::Pong(_) => 12,
+            Message::GetRates(_) => 13,
+            Message::Rates(_) => 14,
+        }
+    }
+}
+
+#[derive(Clone, PartialEq, Eq, Debug)]
+pub struct Version {
+    major: u16, // used only 10 bits
+    minor: u16,
+    patch: u16,
+}
+
+#[derive(Clone, PartialEq, Eq, Debug)]
+pub struct ScanBlock {
+    currency: Currency,
+    version: Version,
+    scan_height: u64,
+    height: u64,
+}
+
 #[derive(Clone, PartialEq, Eq, Debug)]
 pub struct VersionMessage {
-
+    version: Version,
+    time: u64,
+    nonce: [u8; 8],
+    scan_blocks: Vec<ScanBlock>,
 }
 
 #[derive(Clone, PartialEq, Eq, Debug)]
 pub struct FiltersReq {
+    currency: Currency,
+    start: u64,
+    amount: u32,
+}
 
+#[derive(Clone, PartialEq, Eq, Debug)]
+pub struct Filter {
+    block_id: Vec<u8>,
+    filter: Vec<u8>,
 }
 
 #[derive(Clone, PartialEq, Eq, Debug)]
 pub struct FiltersResp {
-
+    currency: Currency,
+    filters: Vec<Filter>
 }
 
 #[derive(Clone, PartialEq, Eq, Debug)]
 pub struct FilterEvent {
-
+    currency: Currency,
+    height: u64,
+    block_id: Vec<u8>,
+    filter: Vec<u8>,
 }
 
 #[derive(Clone, PartialEq, Eq, Debug)]
-pub struct FeeResp {
+pub struct FeeBtc {
+    currency: Currency,
+    fast_conserv: u64,
+    fast_econom: u64,
+    moderate_conserv: u64,
+    moderate_econom: u64,
+    cheap_conserv: u64,
+    cheap_econom: u64,
+}
 
+#[derive(Clone, PartialEq, Eq, Debug)]
+pub struct FeeOther {
+    currency: Currency,
+    fast: u64,
+    moderate: u64,
+    cheap: u64,
+}
+
+#[derive(Clone, PartialEq, Eq, Debug)]
+pub enum FeeResp {
+    FeeBtc(FeeBtc),
+    FeeOther(FeeOther),
+}
+
+#[derive(Clone, PartialEq, Eq, Debug)]
+pub enum RejectData {
+    HeaderParsing,
+    PayloadParsing,
+    InternalError,
+    ZeroBytesReceived,
+    VersionNotSupported,
 }
 
 #[derive(Clone, PartialEq, Eq, Debug)]
 pub struct RejectMessage {
-
+    id: u32,
+    message: String,
+    data: RejectData,
 }
 
 #[derive(Clone, PartialEq, Eq, Debug)]
 pub struct RateReq {
-
+    currency: Currency,
+    fiats: Vec<Fiat>,
 }
 
 #[derive(Clone, PartialEq, Eq, Debug)]
 pub struct RateResp {
-
+    currency: Currency,
+    rates: Vec<(Fiat, FixedU64<U7>)>
 }
